a93cc3128e1d78b4f55b953068cf9cb6ba2aa715
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2021-08-09 16:21:14 +0200

    export functions.
    ATools is a module of general use, and therefore it is convenient to
    export functions so that they can be used without namespace by other
    modules.

--- src/ATools.jl
+++ src/ATools.jl
@@ -1,5 +1,13 @@
 module ATools
 
+export in_range,rxy,phixy,fphi,dxyz,wstd,mean_std  #math
+export select_event,select_by_index,select_by_column_value,
+       select_by_column_value_lt, select_by_column_value_le,
+       select_by_column_value_gt, select_by_column_value_ge,
+       select_by_column_value_interval,select_by_column_value_closed_interval,
+       select_by_column_value_closed_left_interval,select_by_column_value_closed_right_interval,
+       select_by_index,find_max_xy   #util 
+export hist1d, edges, centers, hist2d, p1df #histos  
 include("math.jl")
 include("util.jl")
 include("histos.jl")
