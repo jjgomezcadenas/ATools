c17560fa99a7a8eab9085ed8f91e05e2dd4b2daa
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2021-08-10 21:14:25 +0200

    export after include.

--- src/ATools.jl
+++ src/ATools.jl
@@ -1,5 +1,11 @@
 module ATools
 
+
+include("math.jl")
+include("util.jl")
+include("histos.jl")
+include("fits.jl")
+
 export in_range,rxy,phixy,fphi,dxyz,wstd,mean_std  #math
 export select_event,select_by_index,select_by_column_value,
        select_by_column_value_lt, select_by_column_value_le,
@@ -8,13 +14,9 @@ export select_event,select_by_index,select_by_column_value,
        select_by_column_value_closed_left_interval,select_by_column_value_closed_right_interval,
        select_by_index,find_max_xy   #util 
 export hist1d, edges, centers, hist2d, p1df #histos 
-export lfit, fit_pol1, fit_pol2,fit_pol3, fit_gauss,fit_gauss_fm, #fits
-       fitg1, fitg2, fit_profile
+export lfit, fit_pol1, fit_pol2,fit_pol3, fit_gauss,fit_gauss_fm, 
+       fitg1, fitg2, fit_profile #fits
 
-include("math.jl")
-include("util.jl")
-include("histos.jl")
-include("fits.jl")
 
 end
 
