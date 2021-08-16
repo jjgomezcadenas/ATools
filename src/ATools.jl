e3878558fd1bbf3fed00e73cdeae741bb4750b79
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2021-08-16 20:23:39 +0200

    Add gline2p.

--- src/ATools.jl
+++ src/ATools.jl
@@ -6,8 +6,9 @@ include("util.jl")
 include("histos.jl")
 include("fits.jl")
 
-export in_range,rxy,phixy,fphi,dxyz,wstd,mean_std,fit, fit_pol1, fit_pol2,fit_pol3, fit_gauss,fit_gauss_fm, 
-fitg1, fitg2, fit_profile, plot_fit_gauss  #math
+export in_range,gline2p, rxy,phixy,fphi,dxyz,wstd,mean_std,fit, 
+       fit_pol1, fit_pol2,fit_pol3, fit_gauss,fit_gauss_fm, 
+       fitg1, fitg2, fit_profile, plot_fit_gauss  #math
 export select_event,select_by_index,select_by_column_value,
        select_by_column_value_lt, select_by_column_value_le,
        select_by_column_value_gt, select_by_column_value_ge,
