e0d9705926d465246d2bb6f33f718c44745be0ed
andLaing <a.b.r.laing@gmail.com>
2021-09-17 16:48:17 +0200

    Add question about find_max_xy

--- src/util.jl
+++ src/util.jl
@@ -206,6 +206,10 @@ which the intensity is maximal.
 - `xc::String`: the X column.
 - `yc::String`: the Y column.
 """
+##!! The two functions here have the same name but
+##   one gives the index and x, y where y maximal
+##   and the other index and x, y where x maximal
+##   This behaviour seems unstable.
 function find_max_xy(df::DataFrame, xc::String, yc::String)
 	ymax, imax = findmax(df[!, yc])
 	x_ymax = df[imax, xc]
