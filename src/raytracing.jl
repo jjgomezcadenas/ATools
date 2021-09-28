00522ba7d3210e14925bfe48d96968486b4d1d6c
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2021-09-28 12:02:03 +0200

    Functions for propagation of a ray to a cylinder with tests.

--- src/raytracing.jl
+++ src/raytracing.jl
@@ -15,18 +15,18 @@ From these, a unit vector, u can be computed.
 
 """
 struct Ray
-    p::Vector{Float64}
+    e::Vector{Float64}
     d::Vector{Float64}
     u::Vector{Float64}
 
-    function Ray(p , d)
-        new(p, d, unit_vector(d,p))
+    function Ray(e , d)
+        new(e, d, unit_vector(d,e))
     end
 end
 
 
 """
-    unit_vector(p0::Vector{Real}, p1::Vector{Real})
+    unit_vector(p0::Vector{Float64}, p1::Vector{Float64})
 
 Return a unit vector in the direction defined by points p0 and p1  
 """
@@ -36,14 +36,14 @@ unit_vector(p0::Vector{Float64}, p1::Vector{Float64}) = (p1 - p0) ./ norm(p1 - p
 """
 	propagate_ray(r::Ray)
 
-Return a function that propagates the ray "r" from "p", along "e".
+Return a function that propagates the ray "r" from "e", along "d".
 
 # Fields
 - `r::Ray`  : Ray to be propagated
 """
 function propagate_ray(r::Ray)
     function xray(t::Real)
-        return r.p + t * r.d
+        return r.e + t * r.d
     end
     return xray
 end
@@ -91,35 +91,54 @@ volume(c::Cylinder)= area_endcap(c) * cylinder_length(c)
 in_endcaps(c:: Cylinder, p::Vector{Float64}) = isapprox(p[3], c.zmin) || isapprox(p[3], c.zmax)
     
     
-"""Computes intersection roots between a ray and a cylinder"""
-function intersection_roots(r::Ray, c::Cylinder)
+"""Computes intersection between a ray and a cylinder"""
+function propagate(r::Ray, cy::Cylinder)
     
+    # intersection roots 
     a = r.d[1]^2 + r.d[2]^2
-    b = 2 * (r.p[1] * r.d[1] + r.p[2] * r.d[2])
-    c = r.p[1]^2 + r.p[2]^2 - c.r^2
+    b = 2 * (r.e[1] * r.d[1] + r.e[2] * r.d[2])
+    c = r.e[1]^2 + r.e[2]^2 - cy.r^2
 
-    roots(Polynomial([a, b, -c]))
+    # xray(t) propagates ray along t 
+    xray = ATools.propagate_ray(r)
+
+    # If no real roots return nothing 
+    arg = b^2 - 4*a*c
+
+    # two possible roots 
+    t1 = (-b + sqrt(arg))/(2*a)
+    t2 = (-b - sqrt(arg))/(2*a)
+
+    # Z position of the two roots 
+    z1 = r.e[3] + t1 * r.d[3]
+    z2 = r.e[3] + t2 * r.d[3]
+
+    # belong-to-cylinder condition 
+    zc1 = cy.zmin < z1 < cy.zmax
+    zc2 = cy.zmin < z2 < cy.zmax
+
+
+    if zc1 == false && zc2 == false
+        return nothing
+    elseif zc1 == true && zc2 == false
+        return t1, xray(t1)
+    elseif zc1 == false && zc2 == true
+        return t2, xray(t2)
+    else
+        if t1 > 0 && t2 < 0
+            return t1, xray(t1)
+        elseif t1 < 0 && t2 > 0
+            return t2, xray(t2)
+        elseif t1 > 0 && t2 > 0
+
+            if t1 < t2 
+                return t1, xray(t1)
+            else
+                return t2, xray(t2)
+            end
+        else
+            return nothing
+        end
+    end
 end
-
-
-# def ray_intersection_with_cylinder_end_caps(r: Ray, c: Cylinder, t: float)->np.array:
-#     """Intersection between a ray and the end-cups of a cylinder"""
-#     p = r.ray(t)
-#     if p[2] > c.zmax:
-#         t = (c.zmax - r.e[2])/r.d[2]
-#     else:
-#         t = (c.zmin - r.e[2])/r.d[2]
-
-#     return t, r.ray(t)
-
-
-# def ray_intersection_with_cylinder(r: Ray, c:Cylinder)->Tuple[float,np.array]:
-#     """Intersection between a ray and a cylinder"""
-#     t = cylinder_intersection_roots(r, c)
-#     P = r.ray(t)
-#     z = P[2]
-#     if z < c.zmin or z > c.zmax:
-#         t, P = ray_intersection_with_cylinder_end_caps(r, c, t)
-#     return t, P
-
 
