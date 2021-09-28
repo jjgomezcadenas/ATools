0ccf5e0bdc485f97ac8a2f32177dd9bc9f096e84
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2021-09-28 09:40:21 +0200

    Adding Polynomials for raytracing.

--- src/raytracing.jl
+++ src/raytracing.jl
@@ -1,5 +1,6 @@
 using StatsBase
 using LinearAlgebra
+using Polynomials
 
 """
 	Ray
@@ -29,7 +30,7 @@ end
 
 Return a unit vector in the direction defined by points p0 and p1  
 """
-unit_vector(p0::Vector{Real}, p1::Vector{Real}) = (p1 - p0) ./ mag(p1 - p0)
+unit_vector(p0::Vector{Float64}, p1::Vector{Float64}) = (p1 - p0) ./ norm(p1 - p0)
     
 
 """
@@ -70,18 +71,55 @@ normal_to_barrel(c::Cylinder, p:: Vector{Float64}) = [p[1], p[2], 0.0] ./c.r
 
 equation of cylynder: F(x,y,z) = x^2 + y^2 - r^2 = 0
 """
-cylinder_equation(c::Cylinder, p:: Vector{Float64}) = p[1]^2 + p[2]^2 - c.r^2
+cylinder_equation(c::Cylinder, p::Vector{Float64}) = p[1]^2 + p[2]^2 - c.r^2
 
 
-length(c::Cylinder) = abs.(c.zmax - c.zmin)
+cylinder_length(c::Cylinder) = abs.(c.zmax - c.zmin)
     
 perimeter(c::Cylinder) = 2 * π * c.r
 
-area_barrel(c::Cylinder) = 2 * π * c.r * length(c)
+area_barrel(c::Cylinder) = 2 * π * c.r * cylinder_length(c)
     
 area_endcap(c::Cylinder) = π * c.r^2
     
 area(c::Cylinder) = area_barrel(c) + 2 * area_endcap(c)
 
-volume(c::Cylinder)= area_endcap(c) * length(c)
+volume(c::Cylinder)= area_endcap(c) * cylinder_length(c)
+
+
+"""Returns True if point in end-caps of cyinder"""
+in_endcaps(c:: Cylinder, p::Vector{Float64}) = isapprox(p[3], c.zmin) || isapprox(p[3], c.zmax)
+    
+    
+"""Computes intersection roots between a ray and a cylinder"""
+function intersection_roots(r::Ray, c::Cylinder)
+    
+    a = r.d[1]^2 + r.d[2]^2
+    b = 2 * (r.p[1] * r.d[1] + r.p[2] * r.d[2])
+    c = r.p[1]^2 + r.p[2]^2 - c.r^2
+
+    roots(Polynomial([a, b, -c]))
+end
+
+
+# def ray_intersection_with_cylinder_end_caps(r: Ray, c: Cylinder, t: float)->np.array:
+#     """Intersection between a ray and the end-cups of a cylinder"""
+#     p = r.ray(t)
+#     if p[2] > c.zmax:
+#         t = (c.zmax - r.e[2])/r.d[2]
+#     else:
+#         t = (c.zmin - r.e[2])/r.d[2]
+
+#     return t, r.ray(t)
+
+
+# def ray_intersection_with_cylinder(r: Ray, c:Cylinder)->Tuple[float,np.array]:
+#     """Intersection between a ray and a cylinder"""
+#     t = cylinder_intersection_roots(r, c)
+#     P = r.ray(t)
+#     z = P[2]
+#     if z < c.zmin or z > c.zmax:
+#         t, P = ray_intersection_with_cylinder_end_caps(r, c, t)
+#     return t, P
+
 
--- test/raytracing_test.jl
+++ test/raytracing_test.jl
@@ -1,6 +1,7 @@
 using LinearAlgebra
 @testset "raytracing" begin
     c = ATools.Cylinder(1.0, 0.0, 1.0) 
+    r = ATools.Ray([0.5,0.5,0.5],[1.0,0.0,0.0])
 
     # check that point (1,0,0) verifies cylinder equation 
     @test ATools.cylinder_equation(c, [1.0, 0.0, 0.0]) ≈ 0.0
@@ -12,10 +13,14 @@ using LinearAlgebra
     @test dot(nb,[0.0, 0.0, 1.0]) ≈ 0.0
 
     #check functions for unit cylinder
-    @test ATools.length(c) ≈ 1.0
+    @test ATools.cylinder_length(c) ≈ 1.0
     @test ATools.perimeter(c) ≈ 2 * π 
     @test ATools.area_barrel(c) ≈ 2 * π 
     @test ATools.area_endcap(c) ≈ π 
-    ATools.area(c) ≈ 4 * π
-    ATools.volume(c) ≈ π 
+    @test ATools.area(c) ≈ 4 * π
+    @test ATools.volume(c) ≈ π 
+
+    @test ATools.in_endcaps(c, [1.,1.,0.])
+    @test ATools.in_endcaps(c, [1.,1.,1.]) 
+    println(ATools.intersection_roots(r, c))
 end
