22748d9a81a35db5cf11a7135c2abd17cf8f3e3d
andLaing <a.b.r.laing@gmail.com>
2021-10-05 14:14:52 +0200

    Tidy unnecessary templates from math.jl

--- src/math.jl
+++ src/math.jl
@@ -3,19 +3,19 @@ using StatsBase
 
 
 """
-	rxy(x::T,y::T) where T
+	rxy(x::Real,y::Real)
 	r -> sqrt{x^2 + y^2}
 """
-function rxy(x::T,y::T) where T <: Number
+function rxy(x::Real, y::Real)
     return sqrt(x^2 + y^2)
 end
 
 
 """
-	hixy(x::T,y::T) where T
+	hixy(x::Real, y::Real)
 	phi -> atan(y/x)
 """
-function phixy(x::T,y::T) where T <: Number
+function phixy(x::Real, y::Real)
     return atan(y,x)
 end
 
@@ -29,12 +29,12 @@ end
 
 
 """
-	dxyz(x1::Vector{T}, x2::Vector{T})
+	dxyz(x1::Vector{<:Real}, x2::Vector{<:Real})
 
 Distance between two points.
 """
 
-function dxyz(x1::Vector{T}, x2::Vector{T}) where T <: Number
+function dxyz(x1::Vector{<:Real}, x2::Vector{<:Real})
     return sqrt((x1[1] - x2[1])^2 + (x1[2] - x2[2])^2 + (x1[3] - x2[3])^2)
 end
 
@@ -42,21 +42,21 @@ end
 	gline2p(x1,y1,x2, y2)
 	Line that goes through two points
 """
-function gline2p(x1,y1,x2, y2)
+function gline2p(x1, y1, x2, y2)
     fxy(x) = y1 + (x - x1) * (y2 - y1)/(x2 -x1)
 end
 
-function gline2p(x1::T,y1::T,x2::T, y2::T) where T
-    fxy(x::T) = y1 + (x - x1) * (y2 - y1)/(x2 -x1)
+function gline2p(x1::Real, y1::Real, x2::Real, y2::Real)
+    fxy(x::Real) = y1 + (x - x1) * (y2 - y1)/(x2 -x1)
 end
 
 """
-function wstd(x::Vector{T}, q::Vector{T}) where T
+function wstd(x::Vector{<:Real}, q::Vector{<:Real})
 
 Compute the std deviation in x weighted by q:
 Sqrt(1/Q Sum_i (x - x_mean) * qi )
 """
-function wstd(x::Vector{T}, q::Vector{T}) where T <: Number
+function wstd(x::Vector{<:Real}, q::Vector{<:Real})
 	xmean = mean(x)
 	qs = sum((x.-xmean).^2 .* q)
 	Q = sum(q)
@@ -68,7 +68,7 @@ end
 	mean_std(x, xmin, xmax)
 	Returns mean and std for a vector x in the interval between xmin and xmax
 """
-function mean_std(x::Vector{T}, xmin::T, xmax::T) where T <: Number
+function mean_std(x::Vector{<:Real}, xmin::Real, xmax::Real)
     xx = in_range(x, xmin, xmax)
     xm = mean(xx)
     xs = StatsBase.std(xx)
