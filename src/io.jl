b1d8f91125bbb2dbb3decaa3de47995f274849d8
andLaing <a.b.r.laing@gmail.com>
2021-10-06 18:15:33 +0200

    Add structures for EventParameters (reduced data) output to h5

--- src/io.jl
+++ src/io.jl
@@ -3,6 +3,8 @@ using DataFrames
 using Glob
 using HDF5
 
+import Base.@kwdef
+
 ## These should maybe go in a types file
 """
 	PetaloDF
@@ -36,6 +38,74 @@ struct MlemLor
 end
 
 
+"""
+    EventParameters
+
+Struct representing the parameters for each hemisphere of a selected event.
+@kwdef macro allows for keyword argument definitions, improving readability.
+"""
+@kwdef struct EventParameters
+    event_id ::Int64   = zero(Int64)
+    phot1    ::Bool    = false
+    phot2    ::Bool    = false
+    nsipm1   ::Int64   = zero(Int64)
+    nsipm2   ::Int64   = zero(Int64)
+	q1       ::Float32 = zero(Float32)
+    q2       ::Float32 = zero(Float32)
+	r1       ::Float32 = zero(Float32)
+    r2       ::Float32 = zero(Float32)
+    r1x      ::Float32 = zero(Float32)
+    r2x      ::Float32 = zero(Float32)
+	phistd1  ::Float32 = zero(Float32)
+    phistd2  ::Float32 = zero(Float32)
+	widphi1  ::Float32 = zero(Float32)
+    widphi2  ::Float32 = zero(Float32)
+	zstd1    ::Float32 = zero(Float32)
+    zstd2    ::Float32 = zero(Float32)
+	widz1    ::Float32 = zero(Float32)
+    widz2    ::Float32 = zero(Float32)
+	corrzphi1::Float32 = zero(Float32)
+    corrzphi2::Float32 = zero(Float32)
+	xt1      ::Float32 = zero(Float32)
+    xt2      ::Float32 = zero(Float32)
+	yt1      ::Float32 = zero(Float32)
+    yt2      ::Float32 = zero(Float32)
+	zt1      ::Float32 = zero(Float32)
+    zt2      ::Float32 = zero(Float32)
+	t1       ::Float32 = zero(Float32)
+    t2       ::Float32 = zero(Float32)
+	x1       ::Float32 = zero(Float32)
+    x2       ::Float32 = zero(Float32)
+	y1       ::Float32 = zero(Float32)
+    y2       ::Float32 = zero(Float32)
+	z1       ::Float32 = zero(Float32)
+    z2       ::Float32 = zero(Float32)
+	xr1      ::Float32 = zero(Float32)
+    xr2      ::Float32 = zero(Float32)
+	yr1      ::Float32 = zero(Float32)
+    yr2      ::Float32 = zero(Float32)
+	zr1      ::Float32 = zero(Float32)
+    zr2      ::Float32 = zero(Float32)
+	tr1      ::Float32 = zero(Float32)
+    tr2      ::Float32 = zero(Float32)
+	xb1      ::Float32 = zero(Float32)
+    xb2      ::Float32 = zero(Float32)
+	yb1      ::Float32 = zero(Float32)
+    yb2      ::Float32 = zero(Float32)
+	zb1      ::Float32 = zero(Float32)
+    zb2      ::Float32 = zero(Float32)
+	ta1      ::Float32 = zero(Float32)
+    ta2      ::Float32 = zero(Float32)
+	xs       ::Float32 = zero(Float32)
+    ux       ::Float32 = zero(Float32)
+	ys       ::Float32 = zero(Float32)
+    uy       ::Float32 = zero(Float32)
+	zs       ::Float32 = zero(Float32)
+    uz       ::Float32 = zero(Float32)
+end
+
+
+## Readers/Writers
 """
     readdf(dir)
 
@@ -127,6 +197,20 @@ function read_abc(path::String)
 end
 
 
+"""
+    event_pars_datatype
+
+returns a datatype opbject for saving EventParameters type to hdf5
+"""
+function event_pars_datatype()
+    epdtype = HDF5.h5t_create(HDF5.H5T_COMPOUND, sizeof(EventParameters))
+    epnames = fieldnames(EventParameters)
+    eptypes = fieldtypes(EventParameters)
+    for (i, (nm, typ)) in enumerate(zip(epnames, eptypes))
+        HDF5.h5t_insert(epdtype, nm, fieldoffset(EventParameters, i), datatype(typ))
+    end
+    return HDF5.Datatype(epdtype)
+end
 #"""
 #    dataframe_to_h5()
 #
